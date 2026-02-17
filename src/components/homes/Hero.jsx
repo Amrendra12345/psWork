7902faef61d2b7d0d59b9abf18e93d585de3e655
Amrendra12345 <kumaramarendra834@gmail.com>
2026-02-17 09:45:28 +0530

    hero

--- src/components/homes/Hero.jsx
+++ src/components/homes/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="w-full bg-primary pt-4 pb-2">
         <div className="w-full  mr-0 flex gap-0 items-center relative  flex-col md:flex-row">
-            <div className="w-full md:w-1/2 pl-4 md:pl-20 lg:pl-22">
+            <div className="w-full md:w-1/2 pl-4 md:pl-19">
                 <h1 className="text-[#FFE8DD] text-3xl lg:text-[31px] font-bold leading-12 font-epilogue tracking-wider">Simplifying access to treatment, financial support, and care services</h1>
                 <p className="text-white/80 text-xl my-2">Patient assistance programs, financial guidance, and digital tools — all in one place.</p>
                 <div className="bg-white p-4 rounded max-w-80 mt-8 text-center shadow border-t border-t-gray-500">
